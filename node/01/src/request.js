c4e2b419313cc9d054db3242c1aa102d9c71e4bc
ugrinovich <ugrinovich>
2020-02-20 11:01:28 +0300

    Refactor by review

--- node/01/src/request.js
+++ node/01/src/request.js
@@ -1,17 +1,26 @@
-const fetch = require("node-fetch");
-
+const http = require('http');
+const {s: sync, a: async, repeat = 1} = require('minimist')(process.argv.slice(2))
 const {PORT, HOST} = process.env;
 
-const request = (reqType = 'SYNC') => async (n = 6) => {
-  const url = `http://${HOST}:${PORT}`;
-  for(let i=0; i<n; i++) {
-    if (reqType === 'SYNC') {
-      await fetch(url).then(res => console.log(res.status));
-    } else {
-      fetch(url).then(res => console.log(res.status));
-    }
-  }
-};
+const promisifiedRequest = () => new Promise(resolve => {
+    http.get({
+      hostname: HOST,
+      port: PORT
+    }, (res) => {
+      resolve(res.statusCode)
+    });
+})
 
-module.exports.sync = request('SYNC');
-module.exports.async = request('ASYNC');
+module.exports = (async () => {
+  if (sync) {
+    for(let i=0; i<repeat; i++) {
+      console.log(await promisifiedRequest())
+    }
+  } else if (async) {
+    for(let i=0; i<repeat; i++) {
+      promisifiedRequest().then(code => console.log(code))
+    }
+  } else {
+    console.log('Missing required first argument -a for asynchronous request or -s for synchronous request')
+  }
+})();
